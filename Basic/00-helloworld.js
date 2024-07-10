5200351b7196d1a25944a904a67473d9abaf6902
SamuMichea <sam.micheaa@gmail.com>
2024-07-10 15:50:27 -0400

    primeraClase

---
+++ Basic/00-helloworld.js
@@ -0,0 +1,15 @@
+console.log("HelloWolrd")
+
+/* 
+esto esto
+un comentario 
+en varias lineas
+ */ 
+
+console.log(5)
+console.log(5+8)
+console.log(5-8)
+console.log(5/8)
+console.log(5*8)
+console.log(5**8)
+
---
+++ Basic/01-variables.js
@@ -0,0 +1,30 @@
+// var
+
+//var te permite acceder a una variable antes de ser declarada 
+
+var helloWorld = "¡Hola, JavaScript!"
+console.log(helloWorld)
+
+helloWorld = "¡Hola de nuevo, JavaScript!"
+console.log(helloWorld)
+
+// let
+
+//let no te permite acceder a una variable sin antes ser declarada
+
+let helloWorld2 = "Hola, JavaScript 2"
+console.log(helloWorld2)
+
+helloWorld2 = "Hola de nuevo, JavaScript 2"
+console.log(helloWorld2)
+
+// const
+
+//const no te permite acceder a una variable sin antes ser declarada, ni reutilizar variables
+
+const helloWorld3 = "Hola, JavaScript 3"
+console.log(helloWorld3)
+
+// error
+// helloWorld3 = "Hola de nuevo, JavaScript 3"
+// console.log(helloWorld3)
